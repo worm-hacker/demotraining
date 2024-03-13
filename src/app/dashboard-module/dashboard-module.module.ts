b1ce6bd4c410204b99a2083a176bdec301f8ae36
worm-hacker <manishpatrachar@gmail.com>
2024-03-13 08:27:10 +0530

    group routing, auth guard and redirection

--- src/app/dashboard-module/dashboard-module.module.ts
+++ src/app/dashboard-module/dashboard-module.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    UserListComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatMenuModule,
+    MatButtonModule
   ]
 })
 export class DashboardModuleModule { }
--- src/app/dashboard-module/dashboard-routing.module.ts
+++ src/app/dashboard-module/dashboard-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent}
+  {
+    path: '', component: DashboardComponent,
+    children:[
+      {path:'home', component: HomeComponent},
+      {path:'user-list', component: UserListComponent}
+    ]
+  }
 ];
 
 @NgModule({
